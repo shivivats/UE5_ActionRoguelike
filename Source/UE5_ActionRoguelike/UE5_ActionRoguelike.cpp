f4495db0e44fdfc6b0efd7223ad788b09f31638e
Shivi <shivivats7@gmail.com>
2022-04-08 17:45:20 +0200

    empty project created

---
+++ Source/UE5_ActionRoguelike/UE5_ActionRoguelike.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5_ActionRoguelike.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_ActionRoguelike, "UE5_ActionRoguelike" );
---
+++ Source/UE5_ActionRoguelike/UE5_ActionRoguelikeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5_ActionRoguelikeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_ACTIONROGUELIKE_API AUE5_ActionRoguelikeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
