44b5bf301adc8f52e1066e13d8281668ca907d8e
Shivi <13885576+shivivats@users.noreply.github.com>
2023-10-23 21:50:02 +0200

    partway through lecture 11

---
+++ Source/ActionRoguelike/Private/AI/SBTTaskRangedAttack.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/SBTTaskRangedAttack.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Character.h"
+
+EBTNodeResult::Type USBTTaskRangedAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController* AIController = OwnerComp.GetAIOwner();
+	if(ensure(AIController))
+	{
+		ACharacter* AIPawn = Cast<ACharacter>(AIController->GetPawn());
+		if(AIPawn == nullptr)
+		{
+			return EBTNodeResult::Failed;
+		}
+
+		FVector MuzzleLocation = AIPawn->GetMesh()->GetSocketLocation("Muzzle_01");
+
+		AActor* TargetActor = Cast<AActor>(OwnerComp.GetBlackboardComponent()->GetValueAsObject("TargetActor"));
+		if(TargetActor == nullptr)
+		{
+			return EBTNodeResult::Failed;
+		}
+
+		FVector ShootDirection = TargetActor->GetActorLocation() - MuzzleLocation;
+		FRotator MuzzleRotation = ShootDirection.Rotation();
+
+		FActorSpawnParameters Params;
+		Params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+		AActor* NewProj = GetWorld()->SpawnActor<AActor>(ProjectileClass, MuzzleLocation, MuzzleRotation, Params);
+
+		return NewProj ? EBTNodeResult::Succeeded : EBTNodeResult::Failed;
+	}
+
+	return EBTNodeResult::Failed;
+
+}
---
+++ Source/ActionRoguelike/Public/AI/SBTTaskRangedAttack.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "SBTTaskRangedAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE_API USBTTaskRangedAttack : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory);
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category="AI")
+	TSubclassOf<AActor> ProjectileClass;
+	
+};
